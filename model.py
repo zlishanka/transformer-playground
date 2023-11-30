55301cdb7db5a101c4ffa04792374912df9dea69
Roy Zheng <royzheng@Katies-MacBook-Air-2.local>
2023-11-30 12:25:49 -0800

    Add positional encoding, layer normalization

--- model.py
+++ model.py
@@ -3,24 +3,55 @@ import torch.nn as nn
 import math
 
 class InputEmbeddings(nn.module):
-    
+
     def __init__(self, d_model: int, vocab_size:int):
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
         # mapping: number ---> vector(512)
         self.embedding == nn.Embedding(vocab_size, d_model)
-    
+
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.d_model)
-    
+
 class PositionalEncoding(nn.Module):
-    
+
     def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
         super().__init__()
         self.d_model = d_model
         self.seq_len = seq_len
         self.dropout = nn.Dropout(dropout)
-        
+
         # Create a matrix of shape(seq_len, d_model)
         pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len,1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0)/d_model))
+        # Apply the sin to even position
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # Apply the cos to odd position
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        # require_-grad(False), make this particular tensor not learned
+        x = x + (self.pe[:,:x.shape(1),:].requires_grad(False))
+        return self.dropout(x)
+
+
+class   LayerNormalization(nn.Module):
+
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))    # Multiplied
+        self.bias =  nn.Parameter(torch.ones(1))    # Added
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim=True)
+        std = x.std(dim = -1, keepdim=True)
+        return self.alpha *(x-mean)/(std * self.eps) + self.bias
+
