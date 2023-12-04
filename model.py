f3733b2edb715d7c94aca487bb98e84153dda17f
Roy Zheng <royzheng@Katies-MacBook-Air-2.local>
2023-12-03 23:20:38 -0800

    Fix feed_forward_block definition error

--- model.py
+++ model.py
@@ -167,7 +167,7 @@ class DecoderBlock(nn.Module):
        super().__init__()
        self.self_attention_block = self_attention_block
        self.cross_attention_block = cross_attention_block
-       self.feed_forward_block = FeedForwardBlock
+       self.feed_forward_block = feed_forward_block
        self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(3)])
 
     def forward(self, x, encoder_output, src_mask, tgt_mask):
